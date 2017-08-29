7594fd7ef501bfed49729c8a3a86ec804107dcd1
Nguyen <hinguyen@nrn.nrcan.gc.ca>
2017-08-29 16:26:55 -0400

    implement linear patter

--- Mob.py
+++ Mob.py
@@ -6,8 +6,12 @@ class Mob():
         self._isSubclass()
         self.health = health
         self.pos = None
+        self.width, self.height = 0, 0
+        self.image = None
+        self.move_conductor = None
         self.direction = -1
 
+    # Method to deny intialization of the Mob class
     def _isSubclass(self):
         raise NotImplementedError("Cannot instantiate this class")
 
@@ -15,23 +19,43 @@ class Mob():
         raise NotImplementedError("Subclass must override this method")
 
     def move(self):
-        self.direction = self.next()
+        self.pos = (self.next())
 
-    def draw(self):
+    def draw(self, surface):
         print(self.direction)
+
+    def check_collision(self):
+        pass
+
+    def check_player(self):
+        pass
 
 # Subclass - for each type of Mob
 class Zombie(Mob):
-    def __init__(self, filename, health):
+    def __init__(self, filename, health, movement_speed):
         super().__init__(filename, health)
-        self.pattern = Square_Pattern()
+        self.pattern = Square_Pattern(movement_speed)
 
     def _isSubclass(self): return True
 
+    # TODO : one the player is within a certain radius -> change movement behaviour / speed
     def next(self):
+        # if within radius
+        # note : dont need to check all points of player just a single handle is fine
+            # linear pattern.next()
+        # else:
         return self.pattern.next()
 
-z = Zombie(" world")
-z.draw()
+class Tt():
+    def __init__(self, *args):
+        #self.t = args[0]
+        pass
+
+z = Zombie("", " world", 0)
+z.draw("")
 z.move()
-z.draw()
+z.draw("")
+
+pat = Linear_Pattern(9)
+x, y = pat.next(self_x=0, self_y=0, player_x=10, player_y=10)
+print(x," ",y)
--- NPC_Movement.py
+++ NPC_Movement.py
@@ -1,3 +1,6 @@
+# TODO : check for exception and error if **kwwargs does not contain the variable name required for next()
+import math
+
 class _Movement_Pattern():
     def __init__(self, pattern):
         self.pattern = pattern
@@ -8,14 +11,41 @@ class _Movement_Pattern():
     def __next__(self):
         return self.next()
 
-    def next(self):
+    def next(self, **kwargs):
         raise NotImplementedError("")
 
 class Square_Pattern(_Movement_Pattern):
-    def __init__(self):
+    def __init__(self, movement_speed):
         super().__init__('square_pattern')
+        self.movement_speed = movement_speed
         self.counter = 0
 
-    def next(self):
-        self.counter += 1
+    def next(self, **kwargs):
+        #self_x, self_y = kwargs['self_x'], kwargs['self_y']
+        #width, height = kwargs['width'], kwargs['height']
+        #origin = kwargs['origin']
+
+
+
         return self.counter
+
+class Linear_Pattern(_Movement_Pattern):
+    def __init__(self, movement_speed):
+        super().__init__('linear_patter')
+        self.movement_speed = movement_speed
+
+    def next(self, **kwargs):
+        # move towards other at set movement speed
+        self_x, self_y = kwargs['self_x'], kwargs['self_y']
+        other_x, other_y = kwargs['player_x'], kwargs['player_y']
+
+        x_prime = other_x - self_x
+        y_prime = other_y - self_y
+
+        r_prime = math.sqrt(x_prime * x_prime + y_prime * y_prime)
+
+        # movement speed in pixels?
+        ratio = self.movement_speed / r_prime
+
+        # return vector component normalized to movement speed
+        return self_x + int(x_prime * ratio), self_y + int(y_prime * ratio)
